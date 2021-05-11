ed6a06566e15dd3220e8164238c106ef8541f675
devkiratu <dev.kiratu@gmail.com>
2021-05-11 22:33:59 +0300

    Solve ex 5

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProgrammingExercises
 {
@@ -7,6 +8,27 @@ namespace ProgrammingExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var numbers = new[] {1,7,3,4};
+
+            var sums = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                var sum = 1;
+                for (var i=0; i < numbers.Length; i++)
+                {
+                    sum = num != numbers[i] ? sum *= numbers[i] : sum *= 1; 
+                }
+
+                sums.Add(sum);
+
+            }
+
+
+            foreach (var num in sums)
+                Console.WriteLine(num);
+
         }
     }
 }
