9460a8fb75526212c50b970ce3ab8efc390c2566
devKiratu <dev.kiratu@gmail.com>
2022-05-25 10:58:39 +0300

    optimize solution to Q5 for time and space

--- Exercise5.cs
+++ Exercise5.cs
@@ -7,23 +7,49 @@ namespace ProgrammingExercises
     {
         public static void GetProductOfAllIntsExceptAtIndex(int[] numbers)
         {
-            var sums = new List<int>();
+            #region old solution
+            //var sums = new List<int>();
+
+            //foreach (var num in numbers)
+            //{
+            //    var sum = 1;
+            //    for (var i=0; i<numbers.Length; i++)
+            //    {
+            //        sum = num != numbers[i]? sum *= numbers[i] : sum *= 1; 
+            //    }
+
+            //    sums.Add(sum);
+
+            //}
+
+
+            //foreach (var num in sums)
+            //    Console.WriteLine(num);
+            #endregion
+
+            #region more optimal solution t -> O(n), s -> O(1)
+            //this solution edits the array in place, assumes this is allowed.
             
-            foreach (var num in numbers)
+            //display input for reference
+            Console.Write($"input: [{string.Join(',', numbers)}] ");
+
+            //get the product of all numbers in the array
+            var product = 1;
+            foreach(var num in numbers)
             {
-                var sum = 1;
-                for (var i=0; i<numbers.Length; i++)
-                {
-                    sum = num != numbers[i]? sum *= numbers[i] : sum *= 1; 
-                }
-
-                sums.Add(sum);
-
+                product *= num;
             }
 
+            //generate the products array
+            for(var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = product / numbers[i];
+            }
 
-            foreach (var num in sums)
-                Console.WriteLine(num);
+            //print out the products array alongside initial array
+            Console.Write($"=> products: [{string.Join(',', numbers)}]");
+
+            #endregion
 
         }
 
--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace ProgrammingExercises
         {
             Console.WriteLine("Hello World!");
             //Q5
-            //var numbers = new[] { 1, 7, 3, 4 };
-            //Exercise5.GetProductOfAllIntsExceptAtIndex(numbers);
+            var numbers = new[] { 1, 7, 3, 4 };
+            Exercise5.GetProductOfAllIntsExceptAtIndex(numbers);
 
             //Q4
             var words = new string[]
@@ -31,17 +31,17 @@ namespace ProgrammingExercises
             //var index =  Exercise4.GetIndexOfStartingPoint(words);
             //   Console.WriteLine("You started at word with index" + index);
 
-            //Q3
-            var braces = "{[]()}";
-            var braces1 = "{[(])}";
+            ////Q3
+            //var braces = "{[]()}";
+            //var braces1 = "{[(])}";
 
-            Console.WriteLine(braces + "\t" + Exercise3.IsProperlyNested(braces));
-            Console.WriteLine(braces1 + "\t" + Exercise3.IsProperlyNested(braces1));
+            //Console.WriteLine(braces + "\t" + Exercise3.IsProperlyNested(braces));
+            //Console.WriteLine(braces1 + "\t" + Exercise3.IsProperlyNested(braces1));
 
-            //Q1
-            var flightLength = 90;
-            var movieLengths = new int[] {41, 42, 43, 44, 45, 46};
-            Console.WriteLine(Exercise1.AreThereTwoMovies(flightLength, movieLengths));
+            ////Q1
+            //var flightLength = 90;
+            //var movieLengths = new int[] {41, 42, 43, 44, 45, 46};
+            //Console.WriteLine(Exercise1.AreThereTwoMovies(flightLength, movieLengths));
 
 
         }
