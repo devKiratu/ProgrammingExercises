7ffbf1ec57bf3a7e102ac75e72ee9a832cc86706
devKiratu <dev.kiratu@gmail.com>
2022-05-25 12:45:14 +0300

    add more optimal solution for Q1.

--- Exercise1.cs
+++ Exercise1.cs
@@ -8,21 +8,43 @@ namespace ProgrammingExercises
     {
         public static bool AreThereTwoMovies(int flightLength, int[] movieLengths)
         {
-            var movieLengthSums = new List<int>();
-            foreach (var m in movieLengths)
+            #region old solution, very inefficient t -> >O(n^3)
+            //var movieLengthSums = new List<int>();
+            //foreach (var m in movieLengths)
+            //{
+
+            //    for (var i = 0; i < movieLengths.Length; i++)
+            //    {
+            //        var sum = i != Array.IndexOf(movieLengths, m) ? movieLengths[i] + m : 0;
+            //        if (sum != 0 && !movieLengthSums.Contains(sum))
+            //        {
+            //            movieLengthSums.Add(sum);
+            //        }
+            //    }
+            //}
+
+            //return movieLengthSums.Any(s => s == flightLength);
+            #endregion
+
+            #region more optimal solution t -> O(n)
+            var moviePairs = new Dictionary<int, int>();
+
+            foreach(var m in movieLengths)
             {
-                
-                for (var i = 0; i < movieLengths.Length; i++)
+                //is the current item a pair to a number we've met before?(is it in the list of missing pairs)
+                if(moviePairs.TryGetValue(m, out _))
                 {
-                    var sum = i != Array.IndexOf(movieLengths, m) ? movieLengths[i] + m : 0;
-                    if (sum != 0 && !movieLengthSums.Contains(sum))
-                    {
-                        movieLengthSums.Add(sum);
-                    }
+                    return true;
+                }
+                else
+                {
+                    //add current item's pair to pairs list
+                    var missingPair = flightLength - m;
+                    moviePairs.TryAdd(missingPair,1);
                 }
             }
-
-            return movieLengthSums.Any(s => s == flightLength);
+            return false;
+            #endregion
         }
     }
 }
