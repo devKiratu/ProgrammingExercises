f087e4d264ec31473fd20a2c7dedd62e1b5abcd5
devKiratu <dev.kiratu@gmail.com>
2022-05-26 11:00:43 +0300

    optimize Q4 for space, time not optimal

--- Exercise4.cs
+++ Exercise4.cs
@@ -8,26 +8,44 @@ namespace ProgrammingExercises
         public static int GetIndexOfStartingPoint(string[] words)
         {
             #region initial solution -> t -> O(nlogn) s -> O(n)
-            var helperArray = new string[words.Length];
+            //var helperArray = new string[words.Length];
 
-            for(var i = 0; i < words.Length; i++) //O(n)
-            {
-                helperArray[i] = words[i];
-            }
+            //for(var i = 0; i < words.Length; i++) //O(n)
+            //{
+            //    helperArray[i] = words[i];
+            //}
 
-            Array.Sort(helperArray); //O(nlogn)
+            //Array.Sort(helperArray); //O(nlogn)
 
+            //var index = 0;
+            //for(var i = 0; i < words.Length; i++) //O(n)
+            //{
+            //    if(words[i] == helperArray[0])
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+            //return index;
+            #endregion
+
+            #region solution 2 -> optimizing for space O(1) t >= O(n^2)
             var index = 0;
-            for(var i = 0; i < words.Length; i++) //O(n)
+            var word = words[0];
+
+            for(var i = 1; i < words.Length; i++)
             {
-                if(words[i] == helperArray[0])
+                if(string.Compare( words[i], word) < 0)
                 {
+                    word = words[i];
                     index = i;
-                    break;
                 }
+                
             }
 
             return index;
+            
             #endregion
         }
     }
